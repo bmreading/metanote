9edfe037c39ac781208d78354cf41a87d79f911e
Brian Reading <brian.reading@gmail.com>
2022-01-19 02:06:44 -0600

    Add ability to parse a directory for audio
    This populates the window's sidebar

--- src/window.rs
+++ src/window.rs
@@ -3,9 +3,13 @@ use adw::subclass::prelude::*;
 use gtk::subclass::prelude::*;
 
 use adw::WindowTitle;
+use anyhow::Result;
+use glib::{clone, Object};
 use glib::subclass::InitializingObject;
-use glib::Object;
-use gtk::{CompositeTemplate, ListBox};
+use gio::{File, FileInfo};
+use gtk::{CompositeTemplate, FileChooserAction, FileChooserNative, ListBox, ResponseType};
+use gtk_macros::action;
+use log::warn;
 
 use crate::app::MetanoteApplication;
 
@@ -15,6 +19,8 @@ mod imp {
     #[derive(CompositeTemplate, Default)]
     #[template(resource = "/com/github/bmreading/Metanote/window.ui")]
     pub struct MetanoteApplicationWindow {
+        pub file_chooser: FileChooserNative,
+
         #[template_child]
         pub tracklist: TemplateChild<ListBox>,
         #[template_child]
@@ -27,6 +33,22 @@ mod imp {
         type Type = super::MetanoteApplicationWindow;
         type ParentType = adw::ApplicationWindow;
 
+        fn new() -> Self {
+            // Since FileChooserNative requires a reference to outlive
+            // its invocation closure, we will instantiate it here, and
+            // simply call it later
+            let file_chooser = FileChooserNative::builder()
+                .modal(true)
+                .action(FileChooserAction::SelectFolder)
+                .build();
+
+            Self {
+                file_chooser,
+                tracklist: TemplateChild::default(),
+                main_title: TemplateChild::default(),
+            }
+        }
+
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
         }
@@ -36,7 +58,13 @@ mod imp {
         }
     }
 
-    impl ObjectImpl for MetanoteApplicationWindow {}
+    impl ObjectImpl for MetanoteApplicationWindow {
+        fn constructed(&self, obj: &Self::Type) {
+            self.parent_constructed(obj);
+            obj.setup_actions();
+        }
+    }
+    
     impl WidgetImpl for MetanoteApplicationWindow {}
     impl WindowImpl for MetanoteApplicationWindow {}
     impl ApplicationWindowImpl for MetanoteApplicationWindow {}
@@ -53,5 +81,70 @@ glib::wrapper! {
 impl MetanoteApplicationWindow {
     pub fn new(app: &MetanoteApplication) -> Self {
         Object::new(&[("application", app)]).expect("Failed to create Metanote window")
+    }
+
+    fn setup_actions(&self) {
+        action!(
+            self,
+            "open",
+            clone!(@weak self as window => move |_, _| {
+                let file_chooser = &window.imp().file_chooser;
+                file_chooser.set_transient_for(Some(&window));
+
+                file_chooser.connect_response(clone!(@weak window => move |fc, response| {
+                    if response == ResponseType::Accept {
+                        window.add_tracks(fc.file().expect("Could not retrieve folder from file chooser"));
+                    }
+                    fc.destroy();
+                }));
+
+                file_chooser.show();
+            })
+        );
+    }
+
+    fn add_tracks(&self, dir: File) {
+        let tracklist = &self.imp().tracklist;
+        self.clear_tracklist();
+
+        match self.parse_dir(dir) {
+            Ok(tracks) => {
+                for track in tracks {
+                    let row = adw::ActionRow::builder()
+                        .title("Unknown Artist - Unknown Album")
+                        .subtitle(track.name().to_str().unwrap())
+                        .build();
+                
+                    tracklist.append(&row);
+                }
+            }
+            Err(err) => warn!("unable to parse directory, {err}")
+        }
+    }
+
+    fn clear_tracklist(&self) {
+        let tracklist = &self.imp().tracklist;
+        tracklist.select_all();
+        
+        for track in tracklist.selected_rows() {
+            tracklist.remove(&track);
+        }
+    }
+
+    fn parse_dir(&self, dir: File) ->  Result<Vec<FileInfo>>{
+        let file_enumerator = dir.enumerate_children(
+            "*", 
+            gio::FileQueryInfoFlags::NOFOLLOW_SYMLINKS, 
+            Some(&gio::Cancellable::new()))?;
+
+        let mut audio_tracks = Vec::new();
+        for child in file_enumerator {
+            let file_info = child?;
+            if file_info.content_type().unwrap().contains("audio") {
+                audio_tracks.push(file_info);
+            }            
+        }
+
+        Ok(audio_tracks)
     }
 }
